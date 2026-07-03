34e48704950a228fcfe256b1529884b927eca1eb
tejeshdimmiti5-crypto <tejeshdimmiti5@gmail.com>
2026-07-04 05:27:20 +0530

    Add continueApp function for navigation

--- home.js
+++ home.js
@@ -1,1 +1,18 @@
 
+function continueApp(){
+
+let calculator=document.querySelector('input[name="calc"]:checked').value;
+
+if(calculator==="sgpa"){
+
+window.location.href="sgpa.html";
+
+}
+
+else{
+
+window.location.href="cgpa.html";
+
+}
+
+}
