093e0d9b0942cfa5e58abe7bca8c0ece697937b7
tejeshdimmiti5-crypto <tejeshdimmiti5@gmail.com>
2026-07-04 05:55:00 +0530

    Update subject.js

--- subject.js
+++ subject.js
@@ -1,8 +1,16 @@
-const subjects = [
-    { name: "Programming for Problem Solving", credits: 3 },
-    { name: "Mathematics-I", credits: 4 },
-    { name: "Engineering Physics", credits: 3 },
-    { name: "English", credits: 2 },
-    { name: "PPS Lab", credits: 1.5 },
-    { name: "Physics Lab", credits: 1.5 }
-];
+const subjectData = {
+  "JNTUK": {
+    "R23": {
+      "AI & DS": {
+        "I-I": [
+          { name: "Programming for Problem Solving", credits: 3 },
+          { name: "Mathematics-I", credits: 4 },
+          { name: "Engineering Physics", credits: 3 },
+          { name: "Communicative English", credits: 2 },
+          { name: "PPS Lab", credits: 1.5 },
+          { name: "Physics Lab", credits: 1.5 }
+        ]
+      }
+    }
+  }
+};
