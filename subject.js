caf19a6d0e8da206664efdefadc67d9875b2022e
tejeshdimmiti5-crypto <tejeshdimmiti5@gmail.com>
2026-07-04 05:34:49 +0530

    Change subjects from object to array format

--- subject.js
+++ subject.js
@@ -1,2 +1,8 @@
-
-const subjects = {};
+const subjects = [
+    { name: "Programming for Problem Solving", credits: 3 },
+    { name: "Mathematics-I", credits: 4 },
+    { name: "Engineering Physics", credits: 3 },
+    { name: "English", credits: 2 },
+    { name: "PPS Lab", credits: 1.5 },
+    { name: "Physics Lab", credits: 1.5 }
+];
